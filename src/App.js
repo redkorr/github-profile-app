258d9ae2d8f76e4287562a933f22bad131c9cf52
redkorr <redkor910.97@gmail.com>
2022-07-04 22:35:52 +0200

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { Header, Overview, Repositories } from './components';
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
+  const fetchUserProfile = async () => {
+    const res = await fetch('https://api.github.com/users/redkorr');
+    const data = await res.json();
+
+    setUserData(data);
+  };
+  const [userData, setUserData] = useState(fetchUserProfile);
   return (
-    <div className='App'>
-      <h1>Github Profile App</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Overview userData={userData} />}>
+            <Route path='repositories' element={<Repositories />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
