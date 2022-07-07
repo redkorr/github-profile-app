06193857a679b5eb1161248ee85dc1bdc22dc259
redkorr <redkor910.97@gmail.com>
2022-07-07 17:42:37 +0200

    wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { Searchbar } from '..';
+
 const Header = () => {
   return (
     <div>
       <h1>Github Profile App</h1>
+      <Searchbar></Searchbar>
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header';
 export { default as Overview } from './Overview/Overview';
 export { default as Repositories } from './Repositories/Repositories';
+export { default as Searchbar } from './Searchbar/Searchbar';
