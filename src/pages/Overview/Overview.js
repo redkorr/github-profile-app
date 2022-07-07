f650e68bcb04b9daa6af4ec9eff73e0b840c82da
redkorr <redkor910.97@gmail.com>
2022-07-07 21:11:37 +0200

    created user card

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import DataContext from '../../context/DataContext';
+import { UserCard } from './components';
 
 const Overview = () => {
   const { name } = useParams();
   const { useData } = useContext(DataContext);
   const user = useData(name);
+
   return (
     <div>
-      <h1>{user.login}</h1>
-      <img src={user.avatar_url} />
+      <UserCard user={user} />
     </div>
   );
 };
