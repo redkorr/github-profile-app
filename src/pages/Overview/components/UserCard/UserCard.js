ede0d9631a593c82efe6132a54a5727a999f0201
redkorr <58095758+redkorr@users.noreply.github.com>
2022-07-13 20:02:03 +0200

    user card (#6)
    * added burger-menu icon
    
    * added style for UserCard
    
    * wip

--- src/pages/Overview/components/UserCard/UserCard.js
+++ src/pages/Overview/components/UserCard/UserCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './UserCard.module.css';
 
 const UserCard = ({ user }) => {
   const createDate = new Date(user.created_at);
@@ -9,19 +10,37 @@ const UserCard = ({ user }) => {
     365
   ).toFixed(1);
   return (
-    <div>
-      <h1>{user.login}</h1>
-      <h2>{user.name}</h2>
-      <img src={user.avatar_url} />
-      <p>{user.bio}</p>
-      <div>
-        {user.followers} {user.following}
+    <div className={styles.wrapper}>
+      <div className={styles.userCardUpperSection}>
+        <h1>{user.login}</h1>
+        <h2>{user.name}</h2>
+        <img
+          alt='user avatar'
+          className={styles.avatar}
+          src={user.avatar_url}
+        />
       </div>
-      <p>{user.location}</p>
-      <p>{user.company}</p>
-
-      {user.created_at && <p>{createDate.toISOString().slice(0, 10)} </p>}
-      <p>{difference}</p>
+      <div className={styles.burgerMenuWrapper}>
+        <button className={styles.burgerMenuButton}>
+          <img alt='burger menu icon' src='/burger-menu.svg'></img>
+        </button>
+      </div>
+      <div className={styles.userCardBottomSection}>
+        <div className={styles.text}>
+          <p>Bio: {user.bio}</p>
+          <p>Country: {user.location}</p>
+          <div>
+            <p>
+              Followers: {user.followers} Following: {user.following}
+            </p>
+          </div>
+          <p>Company: {user.company}</p>
+          {user.created_at && (
+            <p>Created: {createDate.toISOString().slice(0, 10)} </p>
+          )}
+          <p>Profile age: {difference}</p>
+        </div>
+      </div>
     </div>
   );
 };
