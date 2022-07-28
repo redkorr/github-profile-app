46163db16d86cc1e6a1be29f96c316ea5effb4c9
redkorr <redkor910.97@gmail.com>
2022-07-28 16:54:17 +0200

    added functionality to buttons

---
+++ src/components/Overview/Overview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overview = () => {
+  return (
+    <div>
+      <p>0</p>
+    </div>
+  );
+};
+
+export default Overview;
--- src/pages/PageLayout/components/DisplayBlock/DisplayBlock.js
+++ src/pages/PageLayout/components/DisplayBlock/DisplayBlock.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import Repositories from '../../../../components/Repositories/Repositories';
+import { Overview, Repositories, Stars } from '../../../../components';
+import DataContext from '../../../../context/DataContext';
 import DisplayBlockHeader from './components/DisplayBlockHeader/DisplayBlockHeader';
 import styles from './DisplayBlock.module.css';
 
 const DisplayBlock = () => {
+  const [index, setIndex] = useState(1);
+  const display = () => {
+    const elements = [<Overview />, <Repositories user={user} />, <Stars />];
+    return elements[index];
+  };
+  const { name } = useParams();
+  const { useData } = useContext(DataContext);
+  const user = useData(name);
   return (
     <div className={styles.wrapper}>
-      <DisplayBlockHeader />
-      <Repositories />
+      <DisplayBlockHeader setIndex={setIndex} />
+      {display()}
     </div>
   );
 };
